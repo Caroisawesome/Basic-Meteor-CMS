f2b7f416b17fc4e6a39f4e7e2957f8c4d48ca815
Carolyn Atterbury <carolynatterbury@gmail.com>
2015-12-28 16:16:44 -0700

    delete link template

--- client/views/links/links.js
+++ client/views/links/links.js
@@ -1,19 +1,22 @@
-const linkEvents = {
-}
-
-Template.getLinkAdd.events(_.extend({
+Template.getLinkAdd.events({
     'click icon': function(e, instance) {
         e.preventDefault();
         if (this.callback) this.callback.bind(this.doc)(e, instance);
     }
 
-},linkEvents));
+});
 
-Template.getLinkEdit.events(_.extend({
+Template.getLinkEdit.events({
     'click icon': function(e, instance) {
         e.preventDefault();
-	console.log('get link edit', this);
-
         if (this.callback) this.callback.bind(this.doc)(e, instance);
     }
-},linkEvents));
+});
+
+
+Template.getLinkRemove.events({
+    'click icon': function(e, instance) {
+        e.preventDefault();
+        if (this.callback) this.callback.bind(this.doc)(e, instance);
+    }
+});			    
